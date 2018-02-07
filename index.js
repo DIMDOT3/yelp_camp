e7605b238771d2a5dc2fce8852b97a66670ad1c5
Joe Dinh <jdinh1985@gmail.com>
2018-02-07 19:08:03 +0000

    add mongo lab

--- index.js
+++ index.js
@@ -33,7 +33,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.use(flash());
 // Connect to DB
-mongoose.connect("mongodb://localhost/yelp_camp");
+// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://admin:admin@ds155934.mlab.com:55934/yelp_camp")
 // Configure passport for authentication
 app.use(require('express-session')({
     secret: 'Once again Rusty wins!',
